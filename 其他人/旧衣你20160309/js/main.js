23bdeca8c4dc86997de2a5838de3b938fe3bf6b0
deark <291236937@qq.com>
2016-03-22 17:28:52 +0800

    update
    更新 我的页面.html

--- 其他人/旧衣你20160309/js/main.js
+++ 其他人/旧衣你20160309/js/main.js
@@ -9,4 +9,23 @@ $(function(){
     $(".dyd-dd .dd-menu li").css("color","#9E9E9E");
     $(".dyd-dd .dd-menu li").eq(num).css("color","#FD602D");
   }
+  /*小色更新 2016-3-12 17：17*/
+  $(".hd-focus-left").click(function(){
+    $(".xs-mask").toggle();
+      $(".hd-focus-left-hover").toggle();
+});
+  $(".hd-focus-right").click(function(){
+    $(".xs-mask").toggle();
+      $(".hd-focus-right-hover").toggle();
+});
+  $(".gb-hide").click(function(){
+    $(".xs-mask").hide(); 
+      $(".hd-focus-left-hover").hide();  
+});
+  $(".xs-mask").click(function(){
+    $(".xs-mask").hide();
+    $(".hd-focus-left-hover").hide(); 
+    $(".hd-focus-right-hover").hide(); 
+});
 })
+
