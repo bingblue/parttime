4136334adc11b88fbb4a38c4c927be84d83bce12
deark <291236937@qq.com>
2016-03-24 13:25:54 +0800

    updated

--- 其他人/旧衣你20160309/js/main.js
+++ 其他人/旧衣你20160309/js/main.js
@@ -1,10 +1,17 @@
+
 $(function() {
-	$(".dyd-dd .dd-menu li").each(function(index) {
-		$(this).click(function() {
-			go(index);
-		})
-	})
-	$(".sure").hide();
+	$(".dyd-dd .dd-menu li").each(function(index){
+    $(this).click(function(){
+      go(index);
+    })
+  })
+  function go(num){
+    $(".dyd-dd .gzdd div").css("display","none").eq(num).css("display","block");
+    $(".dyd-dd .dd-menu li").css("color","#9E9E9E");
+    $(".dyd-dd .dd-menu li").eq(num).css("color","#FD602D");
+  }
+  // 接单
+  $(".sure").hide();
 			$(".down-right").click(function(){
 				$(".sure").show();
 				
@@ -16,7 +23,6 @@ $(function() {
 			$(".sure-up").click(function(){
 				$(".sure").hide();
 			})
-			
 	/*  彭乐 更新 2016-3-23 16:25:24*/
 	$(".btn-2").click(function() {
 		$(".sw-mask").toggle();
