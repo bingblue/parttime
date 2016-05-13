34c8b42a1e571780b9ffb88d92f72561be50d921
xiaomucool <895355044@qq.com>
2016-05-13 13:41:27 +0800

    update 渔夫

--- 3.私活任务/渔夫/js/main.js
+++ 3.私活任务/渔夫/js/main.js
@@ -1,8 +1,4 @@
-var count = 3;
-var countsure = false;
-var time = 15;
-var clickNum = 0;
-var timesure = false;
+var imgNow = 1;
 $(function(){
 	$("body").click(function(){
 		$(".prompt").html("");
@@ -13,49 +9,23 @@ $(function(){
 			event.stopPropagation();
 		}else{
 			event.stopPropagation();
-			$(".prompt").html($("#name").val()+"准备就绪");
-			$(".popup .count").css({display:"block"})
-			countsure = true;
-			var timer = setInterval(function(){
-				if(countsure){
-					--count;
-					$(".popup .count").text(count);
-					if(count == 0){
-						$(".popup .count").text("GO!");
-						clearInterval(timer);
-						$(".popup").css({display:"none"})
-						$(".main .mark").css({display:"none"})
-						timesure = true;
-					}
-				}	
-			},1000);
+			$(".time").html($("#name").val()+" 准备就绪!");
+			$(".popup").hide();
 		}
 	})
-	var t = setInterval(function(){
-		if(timesure){
-			--time;
-			$(".time font").text(time);
-			if(time == 0){
-				clearInterval(t);
-				$.ajax({  
-					type : "post",  
-					url : "",  
-					data : {num:clickNum},  
-					async : true,  
-					success : function(data){  
-						console.log("提交成功！")
-					}  
-				});  
-			}
-		}
-	},1000);
 	$(".jq-an").mutouch({
 		banRight :true,
 		onEnd:function(event){
-			if(timesure){
-				$(".jq-an").attr("src","img/baojian02.png")
-				clickNum++;
-			}
+			imgNow = imgNow==1?2:1;
+			$(".jq-an").attr("src","img/baojian0"+imgNow+".png");
+			$.ajax({  
+				type:"GET",  
+				url:"http://52.9.90.143/education/api/attraction/click?nickname="+$("#name").val(),  
+				async:true,  
+				success:function(data){  
+					console.log("提交成功！")
+				}  
+			}); 
 		}
 	});
 });
